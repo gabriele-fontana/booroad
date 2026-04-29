eb387a5d1d061ec1e153e48154d428c74cfce3d6
Francesca Golzio <francescagolzio@gmail.com>
2026-04-29 17:14:02 +0200

    fix typo

--- src/components/TravelForm.jsx
+++ src/components/TravelForm.jsx
@@ -33,7 +33,7 @@ export default function TravelForm() {
       <div className="accordion-item">
         <h2 className="accordion-header">
           <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Vuoi aggiungere un nuovo tripso?
+            Vuoi aggiungere un nuovo trips?
           </button>
         </h2>
         <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
