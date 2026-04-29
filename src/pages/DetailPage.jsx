953b229c47fb7232284b75d7b7f44229ce5ebc81
gialli24 <a.gialli2409@gmail.com>
2026-04-29 15:27:56 +0200

    Aggiunta variabile di stato customers on DetailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,14 @@
 import TravellersList from "../components/TravellersList";
 import TravelersForm from "../components/TravelersForm";
+import { clienti } from "../data/clienti";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 export default function DetailPage() {
+
+    const { id } = useParams();
+    const [customers, setCustomers] = useState(clienti.filter(cliente => cliente.id_viaggio === parseInt(id)));
+
     return (
         <div className="container py-5">
             <h1>Details</h1>
