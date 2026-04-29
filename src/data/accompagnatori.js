37861c8c85567f42efb6185ba310869709206a63
Francesca Golzio <francescagolzio@gmail.com>
2026-04-29 11:20:07 +0200

    update accompagnatori

--- src/data/accompagnatori.js
+++ src/data/accompagnatori.js
@@ -1,14 +1,14 @@
 const accompagnatori = [
-  { nome: "Federica", cognome: "Lombardi", email: "federica.lombardi@hotmail.it", numero: "333 5637281", ci: "LMBFRC87B42H501E" },
-  { nome: "Stefano", cognome: "Moretti", email: "stefano.moretti@gmail.com", numero: "346 8293746", ci: "MRTSFN84M20G224F" },
-  { nome: "Ilaria", cognome: "Barbieri", email: "ilaria.barbieri@virgilio.it", numero: "389 4738291", ci: "BRBLRI92D44F839G" },
-  { nome: "Roberto", cognome: "Fontana", email: "roberto.fontana@gmail.com", numero: "320 6473829", ci: "FNTRRT80H15C351H" },
-  { nome: "Elisa", cognome: "Santoro", email: "elisa.santoro@libero.it", numero: "375 1837264", ci: "SNTLSE96A45A944I" },
-  { nome: "Nicola", cognome: "Mariani", email: "nicola.mariani@gmail.com", numero: "338 4920173", ci: "MRNNKL83M05H703L" },
-  { nome: "Paola", cognome: "Ferri", email: "paola.ferri@yahoo.it", numero: "365 8374610", ci: "FRRPLA89B46G702M" },
-  { nome: "Gianluca", cognome: "Ferrara", email: "gianluca.ferrara@gmail.com", numero: "391 7263849", ci: "FRRGLC90D10F205N" },
-  { nome: "Silvia", cognome: "Gatti", email: "silvia.gatti@libero.it", numero: "327 4836190", ci: "GTTSLV94C55H501O" },
-  { nome: "Antonio", cognome: "Pellegrini", email: "antonio.pellegrini@gmail.com", numero: "355 9274830", ci: "PLLNTN78M20L219P" },
-]
+  { id: 1, nome: "Federica", cognome: "Lombardi", email: "federica.lombardi@hotmail.it", numero: "333 5637281", ci: "LMBFRC87B42H501E" },
+  { id: 2, nome: "Stefano", cognome: "Moretti", email: "stefano.moretti@gmail.com", numero: "346 8293746", ci: "MRTSFN84M20G224F" },
+  { id: 3, nome: "Ilaria", cognome: "Barbieri", email: "ilaria.barbieri@virgilio.it", numero: "389 4738291", ci: "BRBLRI92D44F839G" },
+  { id: 4, nome: "Roberto", cognome: "Fontana", email: "roberto.fontana@gmail.com", numero: "320 6473829", ci: "FNTRRT80H15C351H" },
+  { id: 5, nome: "Elisa", cognome: "Santoro", email: "elisa.santoro@libero.it", numero: "375 1837264", ci: "SNTLSE96A45A944I" },
+  { id: 6, nome: "Nicola", cognome: "Mariani", email: "nicola.mariani@gmail.com", numero: "338 4920173", ci: "MRNNKL83M05H703L" },
+  { id: 7, nome: "Paola", cognome: "Ferri", email: "paola.ferri@yahoo.it", numero: "365 8374610", ci: "FRRPLA89B46G702M" },
+  { id: 8, nome: "Gianluca", cognome: "Ferrara", email: "gianluca.ferrara@gmail.com", numero: "391 7263849", ci: "FRRGLC90D10F205N" },
+  { id: 9, nome: "Silvia", cognome: "Gatti", email: "silvia.gatti@libero.it", numero: "327 4836190", ci: "GTTSLV94C55H501O" },
+  { id: 10, nome: "Antonio", cognome: "Pellegrini", email: "antonio.pellegrini@gmail.com", numero: "355 9274830", ci: "PLLNTN78M20L219P" },
+];
 
 export default accompagnatori;
