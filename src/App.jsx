6677324dc06a562a26cae9b817798e6a16c66fac
gialli24 <a.gialli2409@gmail.com>
2026-04-29 11:00:35 +0200

    Add routes and pages structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import DetailPage from "./pages/DetailPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/travel/:id" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
