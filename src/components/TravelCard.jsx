a1b64a32ba9f17ee52bae04f29d9ef3ab1a81039
odon-airoldi <odon.airoldi@gmail.com>
2026-04-29 23:30:21 +0200

    add padding card travel

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router"
 export default function TravelCard({ journey }) {
     return (
         <Link to={`/travel/${journey.id}`} className="text-decoration-none cursor-pointer">
-            <div className="bg-white shadow rounded-1 p-3">
+            <div className="bg-white shadow rounded-1 p-5">
                 <div className="row">
                     <div className="col-6">
-                        <h2 className="h5">{journey.destination}</h2>
+                        <h2 className="h5 mb-0">{journey.destination}</h2>
                     </div>
                     <div className="col-6">
-                        <div className="row row-cols-auto gx-2 align-items-center">
+                        <div className="row row-cols-auto gx-2 align-items-center text-secondary">
                             <i className="bi bi-calendar-range"></i>
                             <span>{new Date(journey.start_date).toLocaleDateString("it-IT")}</span>
                             <i className="bi bi-arrow-right"></i>
