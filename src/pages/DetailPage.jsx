c1eabbbbc8f78d542610cfc7e76ea6bde55c0ab6
gialli24 <a.gialli2409@gmail.com>
2026-04-29 16:14:20 +0200

    Add travelCustomers to DetailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,15 +1,23 @@
 import TravellersList from "../components/TravellersList";
 import TravelersForm from "../components/TravelersForm";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function DetailPage({ customers, setCustomers }) {
 
     const { id } = useParams();
 
+    const { travelCustomers, setTravelCustomers } = useState([]);
+
+    useEffect(() => {
+        setTravelCustomers(customers.filter((cliente) => cliente.id_viaggio === parseInt(id)));
+    }, [customers, id]);
+
+
     return (
         <div className="container py-5">
             <h1>Details</h1>
-            <TravellersList />
+            <TravellersList customers={travelCustomers} />
             <TravelersForm customers={customers} setCustomers={setCustomers} />
         </div>
     )
