6047f5666069d9339edae1ea15ec8adff3ec7ea8
Francesca Golzio <francescagolzio@gmail.com>
2026-04-29 17:12:30 +0200

    update variables

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router"
 
-export default function TravelCard({ viaggio }) {
+export default function TravelCard({ trip }) {
     return (
-        <Link to={`/travel/${viaggio.id}`} className="text-decoration-none cursor-pointer">
+        <Link to={`/travel/${trip.id}`} className="text-decoration-none cursor-pointer">
             <div className="bg-white shadow rounded-1 p-3">
                 <div className="row">
-                    <div className="col-6"><h2 className="h5">{viaggio.destinazione}</h2></div>
+                    <div className="col-6"><h2 className="h5">{trip.destinazione}</h2></div>
                     <div className="col-6">
                         <div className="row row-cols-auto gx-2 align-items-center">
                             <i className="bi bi-calendar-range"></i>
-                            <span>{new Date(viaggio.data_inizio).toLocaleDateString("it-IT")}</span>
+                            <span>{new Date(trip.data_inizio).toLocaleDateString("it-IT")}</span>
                             <i className="bi bi-arrow-right"></i>
-                            <span>{new Date(viaggio.data_fine).toLocaleDateString("it-IT")}</span>
+                            <span>{new Date(trip.data_fine).toLocaleDateString("it-IT")}</span>
                         </div>
                     </div>
                 </div>
