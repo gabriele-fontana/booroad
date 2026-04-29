ec8fe27c4ca246a5c480abb385bab531824e2301
odon-airoldi <odon.airoldi@gmail.com>
2026-04-29 15:59:24 +0200

    add title destination in single travel

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,7 @@
 import TravellersList from "../components/TravellersList";
 import TravelersForm from "../components/TravelersForm";
 import { clienti } from "../data/clienti";
+import { viaggi } from "../data/viaggi";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
@@ -8,10 +9,11 @@ export default function DetailPage() {
 
     const { id } = useParams();
     const [customers, setCustomers] = useState(clienti.filter(cliente => cliente.id_viaggio === parseInt(id)));
+    const viaggio = viaggi.find(viaggio => viaggio.id === parseInt(id))
 
     return (
         <div className="container py-5">
-            <h1>Details</h1>
+            <h1>{viaggio.destinazione}</h1>
             <TravellersList customers={customers} />
             <TravelersForm />
         </div>
