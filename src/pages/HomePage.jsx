d0adff00d8e5abfa486aa748fc15f8a77829dd42
odon-airoldi <odon.airoldi@gmail.com>
2026-04-29 12:03:06 +0200

    add render viaggi from data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import viaggi from "../data/viaggi"
+
 export default function HomePage() {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className="container">
+
+            <div>
+                {
+                    viaggi.map(viaggio => (
+                        <div key={viaggio.id} className="p-3">
+                            <h2 className="h5">{viaggio.destinazione}</h2>
+                            <div>{viaggio.data_inizio} {viaggio.data_fine}</div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
