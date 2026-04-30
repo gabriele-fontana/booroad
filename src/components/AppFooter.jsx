626d334d1b7d45601e2ffcdae96bbe030a8373b5
odon-airoldi <odon.airoldi@gmail.com>
2026-04-30 11:17:19 +0200

    add header logo

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,7 +2,7 @@ export default function AppFooter() {
     return (
         <footer className="p-4 text-center mt-3">
             <p className="mb-1 fw-semibold">Booroad</p>
-            <small className="text-muted">© {new Date().getFullYear()} Booroad — All rights reserved.</small>
+            <small className="text-muted">© {new Date().getFullYear()} BooRoad — All rights reserved.</small>
         </footer>
     );
 }
--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -6,7 +6,7 @@ export default function TravelCard({ journey }) {
             <div className="bg-white shadow rounded-1 ps-3 ps-md-5 pe-5 py-3 py-md-5 position-relative">
                 <div className="row align-items-center">
                     <div className="col-md-6">
-                        <h2 className="h5 mb-0">{journey.destination}</h2>
+                        <h2 className="h5 mb-0"><i className="bi bi-geo-alt-fill"></i> {journey.destination}</h2>
                     </div>
                     <div className="col-md-6">
                         <div className="row row-cols-auto gx-2 align-items-center text-secondary">
