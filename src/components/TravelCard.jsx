be5c9ff2c3119307380271f1c374b2167948104f
odon-airoldi <odon.airoldi@gmail.com>
2026-04-30 10:22:15 +0200

    add card responsive

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router"
 export default function TravelCard({ journey }) {
     return (
         <Link to={`/travel/${journey.id}`} className="text-decoration-none cursor-pointer">
-            <div className="bg-white shadow rounded-1 py-5 ps-5 pe-3">
+            <div className="bg-white shadow rounded-1 ps-3 ps-md-5 pe-5 py-3 py-md-5 position-relative">
                 <div className="row align-items-center">
-                    <div className="col-4">
+                    <div className="col-md-6">
                         <h2 className="h5 mb-0">{journey.destination}</h2>
                     </div>
-                    <div className="col">
+                    <div className="col-md-6">
                         <div className="row row-cols-auto gx-2 align-items-center text-secondary">
                             <i className="bi bi-calendar-range"></i>
                             <span>{new Date(journey.start_date).toLocaleDateString("it-IT")}</span>
@@ -16,10 +16,8 @@ export default function TravelCard({ journey }) {
                             <span>{new Date(journey.end_date).toLocaleDateString("it-IT")}</span>
                         </div>
                     </div>
-                    <div className="col-auto">
-                        <i className="fs-3 bi bi-chevron-right"></i>
-                    </div>
                 </div>
+                <i className="position-absolute fs-3 bi bi-chevron-right top-50 end-0 translate-middle-y me-3"></i>
             </div>
         </Link>
     )
