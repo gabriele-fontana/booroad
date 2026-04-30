08306ace8a20e540590c1318e7a8e037b10b94bc
Francesca Golzio <francescagolzio@gmail.com>
2026-04-30 11:50:04 +0200

    implement AppFooter

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,8 +1,8 @@
 export default function AppFooter() {
     return (
         <footer className="p-4 text-center mt-3">
-            <p className="mb-1 fw-semibold">Booroad</p>
-            <small className="text-muted">© {new Date().getFullYear()} BooRoad — All rights reserved.</small>
+            <p className="mb-1 fw-semibold"><i className="bi bi-backpack2"></i> BooRoad</p>
+            <small className="text-muted">{new Date().getFullYear()} &copy; All rights reserved.</small>
         </footer>
     );
 }
