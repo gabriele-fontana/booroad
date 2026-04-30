0b32c2821ef4771a50d0a18f48a4fe2680c4a255
odon-airoldi <odon.airoldi@gmail.com>
2026-04-30 10:11:59 +0200

    add arrow in travel card

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -3,18 +3,21 @@ import { Link } from "react-router"
 export default function TravelCard({ journey }) {
     return (
         <Link to={`/travel/${journey.id}`} className="text-decoration-none cursor-pointer">
-            <div className="bg-white shadow rounded-1 p-5">
-                <div className="row">
-                    <div className="col-6">
+            <div className="bg-white shadow rounded-1 py-5 ps-5 pe-3">
+                <div className="row align-items-center">
+                    <div className="col-4">
                         <h2 className="h5 mb-0">{journey.destination}</h2>
                     </div>
-                    <div className="col-6">
+                    <div className="col">
                         <div className="row row-cols-auto gx-2 align-items-center text-secondary">
                             <i className="bi bi-calendar-range"></i>
                             <span>{new Date(journey.start_date).toLocaleDateString("it-IT")}</span>
                             <i className="bi bi-arrow-right"></i>
                             <span>{new Date(journey.end_date).toLocaleDateString("it-IT")}</span>
                         </div>
+                    </div>
+                    <div className="col-auto">
+                        <i className="fs-3 bi bi-chevron-right"></i>
                     </div>
                 </div>
             </div>
